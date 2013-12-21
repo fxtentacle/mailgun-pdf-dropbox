a3489fa63af5be7255c4a6277cdd35bd443b1e3e
Minh Hieu <ngnmhieu@gmail.com>
2013-12-21 20:16:58 +0700

    implementing ... download, should change Mailbox to singleton?

--- lib/app.rb
+++ lib/app.rb
@@ -1,16 +1,38 @@
+require 'uri'
+require 'open-uri'
 require_relative 'mailgun.rb'
 
 class App
-  # Move to configuration later
-  API_KEY = 'key-21ayc-i-p20k9aw9gcg2toueux58r2p4'   
-  PUB_API_KEY = 'pubkey-3rgvyp7zd408o3y1e179halm4w3pbqu9'
-  DOMAIN = 'sandbox10211.mailgun.org'
+  # Configurations
+  MAIL_API_KEY = 'key-21ayc-i-p20k9aw9gcg2toueux58r2p4'   
+  MAIL_DOMAIN = 'sandbox10211.mailgun.org'
 
   class << self
     def start
-      mail = Mailgun.new(API_KEY, DOMAIN)
-      puts JSON.parse(mail.get_log).keys
+      mail = Mailgun.new(MAIL_API_KEY, MAIL_DOMAIN)
+      # retrieve all stored messages
+      messages = mail.get_messages
+
+      # perform grab link, download, upload to Dropbox
+      messages.each do |message| 
+        body = message['body-plain']
+        subject = message['subject']
+        pdf_urls = extract_pdf_links(body)
+        @dl_files = download(@pdf_urls, subject)
+      end
     end
+
+    # should be private but public make testing easier
+    def download urls, subject 
+      puts urls
+    end
+
+    def extract_pdf_links content
+      urls = URI.extract(content).select do |url|
+        url if url =~ /\.pdf/
+      end if body.is_a? String
+    end
+
   end
 end
 
--- lib/mailgun.rb
+++ lib/mailgun.rb
@@ -1,31 +1,41 @@
 require 'rest-client'
 
-class Mailgun
-
+class Mailgun # TODO: need to handle exceptions, e.g API Requests error, Bad request...
   def initialize api_key, domain
     @api_key, @domain = api_key, domain
   end
 
-  def get_mail
+  # send request message to get stored message, given message url as argument
+  def get_messages 
+    events = JSON.parse(get_events(:stored))
+    msg_urls = events['items'].map do |item|
+      item['storage']['url']
+    end
+
+    messages = msg_urls.map do |url|
+      JSON.parse(RestClient.get url)
+    end
+
+    messages
   end
 
-  def get_log
-    RestClient.get events_url,
-      :params => {
-      :'limit'=>  25,
+
+  private
+  def get_events type
+    RestClient.get events_url, :params => {
+      :event => type.to_s
     }
   end
   
-  private
   def base_url
-    "https://api:#{@api_key}@api.mailgun.net/v2/"
+    "https://api:#{@api_key}@api.mailgun.net/v2"
   end
 
   def events_url
     "#{base_url}/#{@domain}/events"
   end
 
-  def messages_url message_id
-    "#{base_url}/domains/#{@domain}/messages/"
-  end
+  # def messages_url message_id
+  #   "#{base_url}/domains/#{@domain}/messages/#{message_id}"
+  # end
 end
